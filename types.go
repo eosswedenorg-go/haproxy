557a9c1a2f06bd87ad2a4666977642b8f6e3a863
Henrik Hautakoski <henrik@eossweden.org>
2022-01-04 09:11:13 +0100

    Convert hard tabs to soft

--- types.go
+++ types.go
@@ -4,11 +4,11 @@ package haproxy;
 // See https://cbonte.github.io/haproxy-dconv/1.7/configuration.html#5.2-agent-check
 type HealthCheckStatus string
 const (
-	HealthCheckUp 		= "up"
-	HealthCheckDown 	= "down"
-	HealthCheckMaint 	= "maint"
-	HealthCheckReady	= "ready"
-	HealthCheckDrain	= "drain"
-	HealthCheckFailed 	= "failed"
-	HealthCheckStopped 	= "Stopped"
+    HealthCheckUp       = "up"
+    HealthCheckDown     = "down"
+    HealthCheckMaint    = "maint"
+    HealthCheckReady    = "ready"
+    HealthCheckDrain    = "drain"
+    HealthCheckFailed   = "failed"
+    HealthCheckStopped  = "Stopped"
 )

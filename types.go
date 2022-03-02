45868df44834293228d17a2faab7a06f87cc7117
Henrik Hautakoski <henrik@eossweden.org>
2022-03-02 13:00:55 +0100

    types.go: split the statuses into 2 groups (constants and struct) to have a more typesafe api.
    One for statuses that can not have a message and one for statuses that can. both share a common interface (that also implements the Stringer interface)

--- types.go
+++ types.go
@@ -1,14 +1,59 @@
-package haproxy;
-
-// All supported health check values for HAproxy.
 // See https://cbonte.github.io/haproxy-dconv/1.7/configuration.html#5.2-agent-check
-type HealthCheckStatus string
+
+package haproxy
+
+import "fmt"
+
+type HealthCheck interface {
+    String() string
+}
+
+//  Status
+// -----------------------
+
+func Status(s HealthCheckStatusType) HealthCheckStatus {
+    return HealthCheckStatus{Status: s}
+}
+
+type HealthCheckStatusType string
 const (
-    HealthCheckUp       = "up"
-    HealthCheckDown     = "down"
-    HealthCheckMaint    = "maint"
-    HealthCheckReady    = "ready"
-    HealthCheckDrain    = "drain"
-    HealthCheckFailed   = "failed"
-    HealthCheckStopped  = "Stopped"
+    HealthCheckUp       HealthCheckStatusType = "up"
+    HealthCheckMaint    HealthCheckStatusType = "maint"
+    HealthCheckReady    HealthCheckStatusType = "ready"
+    HealthCheckDrain    HealthCheckStatusType = "drain"
 )
+
+type HealthCheckStatus struct {
+    Status HealthCheckStatusType
+}
+
+func (s HealthCheckStatus) String() string {
+    return string(s.Status) + "\n"
+}
+
+
+//  Status Message
+// -----------------------
+
+func StatusMessage(s HealthCheckStatusMessageType, msg string) HealthCheckStatusMessage {
+    return HealthCheckStatusMessage{Status: s, Message: msg}
+}
+
+type HealthCheckStatusMessageType string
+const (
+    HealthCheckDown     HealthCheckStatusMessageType = "down"
+    HealthCheckFailed   HealthCheckStatusMessageType = "failed"
+    HealthCheckStopped  HealthCheckStatusMessageType = "stopped"
+)
+
+type HealthCheckStatusMessage struct {
+    Status HealthCheckStatusMessageType
+    Message string
+}
+
+func (s HealthCheckStatusMessage) String() string {
+    if len(s.Message) < 1 {
+        return string(s.Status) + "\n"
+    }
+    return fmt.Sprintf("%s#%s\n", s.Status, s.Message)
+}
